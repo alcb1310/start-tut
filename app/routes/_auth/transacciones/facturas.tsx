cdb2589b7918ee467ee6d839eb3611dc5a937336
Andres Court <andres@andrescourt.com>
2025-02-09 20:07:11 -0500

    feat: make the routes lazy loading

--- app/routes/_auth/transacciones/facturas.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/_auth/transacciones/facturas")({
-    component: RouteComponent,
-});
-
-function RouteComponent() {
-    return <div>Hello "/_auth/transacciones/facturas"!</div>;
-}
--- app/routes/_auth/transacciones/presupuesto.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/_auth/transacciones/presupuesto')({
-  component: RouteComponent,
-})
-
-function RouteComponent() {
-  return <div>Hello "/_auth/transacciones/presupuesto"!</div>
-}
